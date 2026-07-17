83485dcd6a41efe3fbb121794f98013a100f71b3
Rangga Mukti <r4n994mukti@gmail.com>
2026-07-17 19:26:37 +0700

    ADD feature: setup FindingHistoryController controller

---
+++ app/Http/Controllers/FindingHistoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FindingHistory;
+use Illuminate\Http\Request;
+
+class FindingHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FindingHistory $findingHistory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FindingHistory $findingHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FindingHistory $findingHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FindingHistory $findingHistory)
+    {
+        //
+    }
+}
